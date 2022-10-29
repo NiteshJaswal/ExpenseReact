4d2254e13fd65fd2c89173fc3dc8be2b1354f994
Nitesh jaswal <104274323+NiteshJaswal@users.noreply.github.com>
2022-10-29 10:32:48 +0530

    Update Expenses.js

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -18,26 +18,7 @@ return( <Card className = "expenses">
                   )
             )
       }
- {/* <ExpenseItems 
-       date= {props.item[0].date}
-       title = {props.item[0].title} 
-       amount = {props.item[0].amount}
-        /> 
-        <ExpenseItems  
-       date= {props.item[1].date}
-       title = {props.item[1].title} 
-       amount = {props.item[1].amount}/>
-       
-        <ExpenseItems 
-        date= {props.item[2].date}
-        title = {props.item[2].title} 
-        amount = {props.item[2].amount}/>
-       
-        <ExpenseItems 
-        date= {props.item[3].date}
-        title = {props.item[3].title} 
-        amount = {props.item[3].amount} />
-        */}
+
       
 </Card>
 

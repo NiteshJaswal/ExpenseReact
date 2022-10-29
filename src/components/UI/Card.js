de35e78471d662484813c1933a4e786f818cfcd5
Nitesh jaswal <104274323+NiteshJaswal@users.noreply.github.com>
2022-10-29 10:37:44 +0530

    Update Card.js

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,12 +1,5 @@
 
-// import './Card.css';
 
-// const Card = (props) =>{
-//     const classes = 'card ' + props.className ;            //' ' space inside this
-// return  <div className= {classes}> {props.children}</div>
-
-// }
-// export default Card;
 import React from 'react';
 
 import './Card.css';
